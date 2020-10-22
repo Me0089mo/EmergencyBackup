f5d3581cbf14e38d2c2f7f89d8bfca313f1f0b63
Me0089mo <31458680+Me0089mo@users.noreply.github.com>
2020-10-22 15:04:15 -0500

    Pantallas de registro de usuario

---
+++ V1.0/app/src/main/java/com/example/emergencybackupv10/SignIn2.kt
@@ -0,0 +1,12 @@
+package com.example.emergencybackupv10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignIn2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in2)
+    }
+}
