e5c7b79c9d940c96003de3989a3cef1470cad549
Damian <repo@cnrservicios.com.mx>
2020-11-17 20:52:32 -0600

    Conexión a la base de datos

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,28 @@
-//Express
+//Express server
 const express = require("express");
 const app = express();
 const cors = require("cors");
+//Enviromental Variables
 const dotenv = require('dotenv');
+dotenv.config();
+//Database conection
 const mongoose = require("mongoose");
+mongoose.connect(process.env.DB_CONECCTION, {useNewUrlParser: true});
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+
+db.once("open", function() {
+  console.log("Connection Successful!");
+});
+
 //Import routes 
 const authRoute = require('./routes/auth');
-dotenv.config();
-
 //Middleware
 app.use(express.json());
 //Route middleware
 app.use('/api/users',authRoute);
+
 
 const port = process.env.port || 5000;
 app.use(express.json());
