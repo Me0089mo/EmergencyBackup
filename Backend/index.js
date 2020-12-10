7c9ae1ce530fff1671143d770f193aad4d22aa35
Ubuntu <TT2020-A061@TT2020-A061.0cjjz1xetdtelctb0ilqxs35rf.bx.internal.cloudapp.net>
2020-12-10 00:56:10 +0000

    cambios a la configuración del puerto

--- Backend/index.js
+++ Backend/index.js
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use('/api/users',authRoute);
 
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
