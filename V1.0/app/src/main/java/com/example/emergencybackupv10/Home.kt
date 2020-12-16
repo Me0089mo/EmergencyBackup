6a8a3823d9d4df5490b54ac80750938c30fb8aa9
Damián <asdf1234damian@gmail.com>
2020-12-16 00:14:37 -0600

    Log in automatico y server nuevo x2

--- V1.0/app/src/main/java/com/example/emergencybackupv10/Home.kt
+++ V1.0/app/src/main/java/com/example/emergencybackupv10/Home.kt
@@ -1,15 +1,24 @@
 package com.example.emergencybackupv10
 
+import android.app.Activity
+import android.content.Intent
+import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
+import androidx.preference.PreferenceManager
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
+import com.auth0.android.jwt.JWT
 import com.example.emergencybackupv10.fragments.HomeFragment
 import com.example.emergencybackupv10.fragments.RestoreFragment
 import com.example.emergencybackupv10.fragments.SettingsFragment
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.fragment_settings.*
+import java.io.File
 
 
 class Home : AppCompatActivity() {
@@ -19,13 +28,21 @@ class Home : AppCompatActivity() {
     private val homeFragment = HomeFragment()
     private val restoreFragment = RestoreFragment()
     private val settingsFragment = SettingsFragment()
+    private lateinit var cipheredDataPath: String;
+    private lateinit var sharedPreferences:SharedPreferences;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        backUpOnCloud = intent.getBooleanExtra(R.string.ARG_BU_AVAILABLE.toString(), false)
-        username = intent.getStringExtra(R.string.ARG_NAME.toString())
-        id = intent.getStringExtra(R.string.ARG_ID.toString())
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val wasLogedIn =intent.getBooleanExtra(getString(R.string.CONFIG_WAS_LOGED_IN), false)
+
+        if(wasLogedIn){
+            getDataFromServer()
+        }else{
+            getDataFromIntent();
+        }
+
         changeFragment(homeFragment)
         bottom_nav.selectedItemId = R.id.navigation_home
         bottom_nav.setOnNavigationItemSelectedListener {
@@ -36,6 +53,20 @@ class Home : AppCompatActivity() {
             }
             true
         }
+    }
+
+    private fun getDataFromIntent(){
+        backUpOnCloud = intent.getBooleanExtra(getString(R.string.ARG_BU_AVAILABLE), false)
+        username = intent.getStringExtra(getString(R.string.ARG_NAME))
+        id = intent.getStringExtra(getString(R.string.ARG_ID))
+    }
+
+    private fun getDataFromServer(){
+        val t = sharedPreferences.getString(getString(R.string.CONFIG_TOKEN),null)!!
+        val jwt = JWT(t)
+        username = jwt.getClaim(getString(R.string.ARG_NAME)).asString()
+        id = jwt.getClaim(getString(R.string.ARG_ID)).asString()
+        backUpOnCloud = false
     }
 
     private fun changeFragment(fragment: Fragment) {
@@ -69,4 +100,12 @@ class Home : AppCompatActivity() {
         navHostFragment.arguments = bundle
     }
 
+    fun logOut(v: View){
+        with (sharedPreferences.edit()) {
+            remove(getString(com.example.emergencybackupv10.R.string.CONFIG_TOKEN))
+            apply()
+        }
+        val intent = Intent(this, Login::class.java)
+        startActivity(intent)
+    }
 }
