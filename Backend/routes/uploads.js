651bf39be55ed74826a895fc89f61bbe9f7d5646
Damián <asdf1234damian@gmail.com>
2021-01-14 03:05:51 +0000

    pendiente de correccion

--- Backend/routes/uploads.js
+++ Backend/routes/uploads.js
@@ -1,9 +1,33 @@
 const router = require("express").Router();
-var multer  = require('multer');
-var upload = multer({ dest: 'uploads/' })
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const multer  = require('multer');
+const fs = require('fs');
 
-
-router.post('/', upload.single('avatar'), function (req, res, next) {
-  // req.file is the `avatar` file
-  // req.body will hold the text fields, if there were any
+var storage = multer.diskStorage({
+	destination: function(req,file,cb){
+		cb(null,'uploads')},
+	filename:function(req,file,cb){
+		const decoded = jwt.verify(req.body.auth,process.env.PRIVATE_KEY)
+		const user = decoded._id
+		const dir = "uploads/"+user
+		console.log(typeof file.buffer);
+		if (!fs.existsSync(dir)){
+			fs.mkdirSync(dir)
+		}
+		cb(null,"/"+user+"/"+file.originalname)}
 })
+
+//var storage = multer.memoryStorage()
+
+var upload  = multer({storage:storage})
+
+router.post('/', upload.single('file'), function (req, res, next) {
+const decoded = jwt.verify(req.body.auth,process.env.PRIVATE_KEY)
+const userID = decoded._id
+console.log('File being uploaded from:'+userID)
+
+//console.log(req.file.buffer.toString("utf-8", 0, 32))
+//console.log(req.file.buffer.toString("utf-8",0,33))
+}
+module.exports = router
