2231b2f5cbc2ccf84ed6947e2fcfa1114e7bc717
Damian <asdf1234damian@gmail.com>
2021-05-27 16:30:45 -0500

    Habilitado https

--- Backend/index.js
+++ Backend/index.js
@@ -1,46 +1,53 @@
 //Express server
 const express = require("express");
 const app = express();
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 const cors = require("cors");
-const morgan = require('morgan')
+const morgan = require("morgan");
 //Enable CORS
-app.use(cors())
-
+app.use(cors());
+//Enable HTTPS
+var http = require("http");
+var https = require("https");
+var privateKey = fs.readFileSync("sslcert/server.key", "utf8");
+var certificate = fs.readFileSync("sslcert/server.crt", "utf8");
+var credentials = { key: privateKey, cert: certificate };
 //Logs
-const accesLogStream = fs.createWriteStream(path.join(__dirname,'access.log'),{flags:'a'})
-app.use(morgan('dev',{stream:accesLogStream}))
+const accesLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+app.use(morgan("dev", { stream: accesLogStream }));
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 //Enviromental Variables
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 //Database conection
 const mongoose = require("mongoose");
-mongoose.connect(process.env.DB_CONECCTION, {useNewUrlParser: true});
+mongoose.connect(process.env.DB_CONECCTION, { useNewUrlParser: true });
 const db = mongoose.connection;
-
 
 db.on("error", console.error.bind(console, "connection error:"));
 
-db.once("open", function() {
+db.once("open", function () {
   console.log("Connection Successful!");
 });
 
-//Import routes 
-const authRoute = require('./routes/auth');
-const downloadsRoute = require('./routes/download');
-const uploadsRoute = require('./routes/uploads');
+//Import routes
+const authRoute = require("./routes/auth");
+const downloadsRoute = require("./routes/download");
+const uploadsRoute = require("./routes/uploads");
 const { required } = require("joi");
 //Middleware
 app.use(express.json());
 //Route middleware
-app.use('/api/users',authRoute);
-app.use('/api/upload',uploadsRoute);
+app.use("/api/users", authRoute);
+app.use("/api/upload", uploadsRoute);
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+httpServer.listen(8080);
+httpsServer.listen(8443);
